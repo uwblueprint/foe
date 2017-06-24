db1e38f3e3faeabea738a5aeae98d6ad4f21cdc1
Dinah Shi <dinahshi28@gmail.com>
2017-06-24 09:29:02 -0400

    Disable hitting facebook api

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -4,13 +4,13 @@ class AuthController < ApplicationController
   def create
     render json: { token: "static_token" }
 
-    if params[:provider] != "facebook"
-      return render json: {error: :provider_not_supported}, status: bad_request
-    end
-
-    user_info, access_token = Omniauth::Facebook.authenticate(params[:code])
-    @current_user = User.find_or_create_by!(oauth_token: access_token)
-    @current_user.update!(user_info)
-    render json: { token: @current_user.token }
+    # if params[:provider] != "facebook"
+    #   return render json: {error: :provider_not_supported}, status: bad_request
+    # end
+    #
+    # user_info, access_token = Omniauth::Facebook.authenticate(params[:code])
+    # @current_user = User.find_or_create_by!(oauth_token: access_token)
+    # @current_user.update!(user_info)
+    # render json: { token: @current_user.token }
   end
 end

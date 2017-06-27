764aad81f3acf8d0d9137aa8723025c5ec5c226d
Dinah Shi <dinahshi28@gmail.com>
2017-06-27 13:43:48 -0400

    Respond 401 on OAuthToken exceptions

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,10 +3,14 @@ class AuthController < ApplicationController
 
   def create
     if params[:provider] != "facebook"
-      return render json: {error: :provider_not_supported}, status: :bad_request
+      return render json: { error: :provider_not_supported }, status: :bad_request
     end
 
-    @current_user = User.from_code(params[:code])
-    render json: { token: @current_user.token }
+    begin
+      @current_user = User.from_code(params[:code])
+      render json: { token: @current_user.token }
+    rescue
+      return render json: { error: :oauth_code_unauthorized }, status: 401
+    end
   end
 end
--- test/controllers/auth_controller_test.rb
+++ test/controllers/auth_controller_test.rb
@@ -5,10 +5,20 @@ class AuthControllerTest < ActionDispatch::IntegrationTest
     @user = users(:one)
   end
 
-  test "should throw error for invalid provider" do
+  test "should respond 400 for invalid provider" do
     post "/auth/invalid", params: { code: "1234" }
     assert_response 400
     assert_includes @response.body, "provider_not_supported"
+  end
+
+  test "should respond 401 for authentication exception" do
+    raises_exception = -> { raise Koala::Facebook::OAuthTokenRequestError.new }
+
+    User.stub :from_code, raises_exception do
+      post "/auth/facebook", params: { code: "1234" }
+      assert_response 401
+      assert_includes @response.body, "oauth_code_unauthorized"
+    end
   end
 
   test "should return successfully with token" do

3b650b76a9b206247db1326a050dbf1cd7b1bab3
Dinah Shi <dinahshi28@gmail.com>
2018-06-19 09:09:03 -0400

    Add street address to Sighting model
    Use street address to cache name of location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_28_191839) do
+ActiveRecord::Schema.define(version: 2018_06_15_204312) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2018_04_28_191839) do
     t.string "species"
     t.float "latitude", null: false
     t.float "longitude", null: false
+    t.string "street_address"
     t.index ["user_id"], name: "index_sightings_on_user_id"
   end
 

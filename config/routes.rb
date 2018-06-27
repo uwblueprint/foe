33d69f05165e35dd8375ddb0f52b51e22dd5b843
Dinah Shi <dinahshi28@gmail.com>
2018-06-27 14:58:38 -0400

    Add resend_confirmation endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
 
+  namespace :auth do
+    resources :resend_confirmation, only: [:create]
+  end
+
   resources :sightings
 end

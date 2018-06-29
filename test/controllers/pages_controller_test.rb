16ae6529bddf77654a8b3f12caf3f7d638c90dc3
Dinah Shi <dinahshi28@gmail.com>
2018-06-28 21:01:50 -0400

    Style confirmation instructions email

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get confirm" do
-    get pages_confirm_url
+    get confirm_url
     assert_response :success
   end
 end
